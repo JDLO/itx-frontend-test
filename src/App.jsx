43ffb2fe5c4e17bc8f8393e7b6c2a984ca67cb31
JDLO <juandavidleono@gmail.com>
2026-06-07 21:49:40 +0200

    Added components to use in the main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import ProductDetailsPage from './views/ProudctDetailsPage'
+import ProductListPage from './views/ProductListPage'
+import { CartProvider } from './context/CartContext'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './componets/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,8 +18,8 @@ function App() {
         <Header />
         <main className="container">
           <Routes>
-            <Route path="/" element={<PLPView />} />
-            <Route path="/product/:id" element={<PDPView />} />
+            <Route path="/" element={<ProductListPage />} />
+            <Route path="/product/:id" element={<ProductDetailsPage />} />
           </Routes>
         </main>
       </BrowserRouter>
