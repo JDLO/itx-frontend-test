20bb20bb9008a8ee0b38907c3522de62e153216c
JDLO <juandavidleono@gmail.com>
2026-06-09 16:05:26 +0200

    Added a test-setup to use in the tests

---
+++ src/test-setup.js
@@ -0,0 +1,11 @@
+if (typeof localStorage === 'undefined' || localStorage === null) {
+  const store = new Map();
+  globalThis.localStorage = {
+    getItem: (key) => store.get(key) ?? null,
+    setItem: (key, value) => store.set(key, String(value)),
+    removeItem: (key) => store.delete(key),
+    clear: () => store.clear(),
+    get length() { return store.size; },
+    key: (index) => [...store.keys()][index] ?? null,
+  };
+}
