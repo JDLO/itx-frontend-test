9941ce7313db90a3dcf914639103a73bbaf86ed3
Juan David Leon <juandavidleono@gmail.com>
2026-06-10 22:22:44 +0200

    Added new tests for product service

--- src/services/product.test.jsx
+++ src/services/product.test.jsx
@@ -56,4 +56,44 @@ describe('productService', () => {
     await expect(productService.getProducts()).rejects.toThrow('Error al obtener productos');
   });
 
+  it('getProductById should fetch product by id', async () => {
+    const mockProduct = { id: '0001', brand: 'Acer', model: 'Liquid', price: '100' };
+
+    vi.spyOn(window, 'fetch').mockResolvedValue({
+      ok: true,
+      json: async () => mockProduct,
+    });
+
+    const result = await productService.getProductById('0001');
+
+    expect(result).toEqual(mockProduct);
+    expect(window.fetch).toHaveBeenCalledWith('/api/product/0001');
+
+    const cacheData = cacheManager.get('prodcut_detail_0001');
+    expect(cacheData).toEqual(mockProduct);
+  });
+
+  it('getProductById should return cached data if available', async () => {
+    const mockProduct = { id: '0001', brand: 'Acer', model: 'Liquid', price: '100' };
+
+    vi.spyOn(window, 'fetch').mockResolvedValue({
+      ok: true,
+      json: async () => mockProduct,
+    });
+
+    await productService.getProductById('0001');
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+
+    const result = await productService.getProductById('0001');
+    expect(result).toEqual(mockProduct);
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('getProductById should throw error if response is not ok', async () => {
+    vi.spyOn(window, 'fetch').mockResolvedValue({
+      ok: false
+    });
+
+    await expect(productService.getProductById('0001')).rejects.toThrow('Error al obtener el detalle del producto');
+  });
 })
